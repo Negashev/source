0b182ab048c2ef7d2d123e4f759d9387f42c95b2
Negash <i@negash.ru>
2016-10-20 15:16:03 +0500

    Update entrypoint.sh

--- gitlab/entrypoint.sh
+++ gitlab/entrypoint.sh
@@ -1,2 +1,2 @@
-gitlab-runner register --non-interactive --url "$CI_SERVER_URL" --registration-token "$CI_SERVER_URL" "$@"
+gitlab-runner register --non-interactive --url "$CI_SERVER_URL" --registration-token "$REGISTRATION_TOKEN" "$@"
 /usr/bin/dumb-init /entrypoint run --user=gitlab-runner --working-directory=/home/gitlab-runner
