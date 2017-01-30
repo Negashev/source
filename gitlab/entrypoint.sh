3e55cc60d74b5836b40103ec4b72caed0f044dfd
Negash <i@negash.ru>
2017-01-30 16:17:47 +0500

    Update entrypoint.sh

--- gitlab/entrypoint.sh
+++ gitlab/entrypoint.sh
@@ -1,2 +1,3 @@
 [ -f /etc/gitlab-runner/config.toml ] && echo "exist /etc/gitlab-runner/config.toml" || gitlab-runner register --non-interactive --url "$CI_SERVER_URL" --registration-token "$REGISTRATION_TOKEN" "$@"
+echo $REGISTRY > /usr/local/share/ca-certificates/gitlab.crt
 /usr/bin/dumb-init /entrypoint run --user=gitlab-runner --working-directory=/home/gitlab-runner
