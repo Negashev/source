2620ea59f91da07e5cbc00fd4e6df7a3c7b84831
Negash <i@negash.ru>
2017-05-14 17:49:38 +0500

    Update mirror.sh

--- minio/mirror.sh
+++ minio/mirror.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 mc config host add minio http://lb.minio.rancher.internal:9000 minio minio123 S3v4
-mc mirror --force --remove --watch /load/ minio/dht/${HOSTNAME}
+mc mirror --force --remove --watch /load/ minio/${BUCKET}/${HOSTNAME}
